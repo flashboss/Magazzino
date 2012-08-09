d621cf8770ee2ea5f73b9de75fdc69344e0d89b8
Luca Stancapiano <jedim@vige.it>
2012-08-09 22:20:22 +0200

    #2 - Moved the Dependencies. It is a common class

--- src/test/java/it/vige/magazzino/test/Dependencies.java
+++ src/test/java/it/vige/magazzino/test/Dependencies.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package it.vige.magazzino.test.persistence;
+package it.vige.magazzino.test;
 
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.GenericArchive;
